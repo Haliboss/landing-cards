d394151ec84a77422fb89d5e9a58939d677c10be
Haliboss <halibos@gmail.com>
2022-10-15 14:42:51 +0300

    add

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,19 +5,19 @@ import Col from "react-bootstrap/Col";
 import { data } from "../helpers/data";
 
 const Card = () => {
+  console.log(data);
   return (
-    <Container className="rounded-4 mt-4" style={{ background: "#f48b29" }}>
-      <h1>Language</h1>
-      <Row>
+    <Container className="rounded-4 mt-4 p-4" style={{ background: "#f48b29" }}>
+      <h1 className="text-white my-2">Language</h1>
+      <Row className="g-3 justify-content-center">
         {data.map((lang, index) => {
           return (
             <Col sm={6} md={4} lg={3} key={index}>
-              <Language {...lang}  />
+              <Language {...lang} />
             </Col>
-          )
+          );
         })}
       </Row>
-
     </Container>
   );
 };
